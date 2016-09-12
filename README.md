9c23c294d62f5f017edc4768597e356b1eedc05b
Fedor Korshunov <mail@fedor.cc>
2016-09-13 00:21:07 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,67 @@
+## Quick start
+
+```js
+// config.js
+module.exports = {
+	mongo: {
+		$init: function (params) {
+			// returns a Promise
+			return require('mongodb').MongoClient.connect(
+				'mongodb://' + params.host + ':' + params.port + '/' params.db
+			)
+		},
+		$default: {
+			db: 'test',
+			port: 27017
+		},
+		$development: {
+			host: '127.0.0.1'
+		},
+		$production: {
+			db: 'prod',
+			host: '192.168.0.10'
+		}
+	},
+	redis: {
+		$init: function (params) {
+			return require('redis').createClient(params)
+		},
+		$default: {
+			port: 6379
+		},
+		$development: {
+			host: '127.0.0.1'
+		},
+		$production: {
+			host: '192.168.0.11'
+		}
+	}
+}
+```
+
+```js
+// index.js
+require('config2')({config: require(./config)}).then(
+	function(config) {
+		console.log('ENV', config.env)
+		
+		console.log('Started mongo on', config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.db)
+		console.log('Started redis on', config.redis.host + ':' + config.redis.port)
+		
+		config.mongo.$instance.collection('documents').insert({hello: 'world'})
+		config.redis.$instance.set('hello', 'world')
+	}
+)
+```
+
+```
+> node index.js
+ENV development
+Started mongo on 127.0.0.1:27017/test
+Started redis on 127.0.0.1:6379
+
+> NODE_ENV=production node index.js
+ENV production
+Started mongo on 192.168.0.10:27017/prod
+Started redis on 192.168.0.11:6379
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+var maskKeys = ['secret', 'token', 'key', 'pass', 'pwd']
+module.exports = require('co').wrap(function* (params) {
+	var get = function (val, def) {
+		return (val !== undefined) ? val : def
+	}
+
+	var env           = (process.env.NODE_ENV || 'development').toLowerCase()
+	var config        = params.config
+	var resourcesList = Object.keys(config)
+	var init          = get(params.init, resourcesList)
+	var verbose       = get(params.verbose, false)
+	var maskSecrets   = get(params.mask_secrets, true)
+
+	var mask = function (key, val) {
+		if (!maskSecrets) {
+			return val;
+		}
+		var matches = maskKeys.filter(function(maskKey) {
+			return (key.toLowerCase().indexOf(maskKey) !== -1)
+		})
+		if (matches.length) {
+			return '********************'
+		}
+		return val
+	}
+
+	var result = {}
+	for (var i = 0; i < resourcesList.length; i++) {
+		var resourceName   = resourcesList[i]
+		var resource       = config[resourceName]
+		var default_config = resource.$default || {}
+		var env_config     = resource['$' + env] || {}
+		result[resourceName] = Object.assign({}, default_config, env_config)
+	}
+	for (i = 0; i < init.length; i++) {
+		var resourceName = init[i]
+		var resource = config[resourceName]
+		if (!resource.$init) {
+			continue
+		}
+		resourceConfig = result[resourceName]
+		if (verbose) {
+			console.log('[' + resourceName + ']: starting')
+			Object.keys(resourceConfig).forEach(function (key) {
+				var value = mask(key, resourceConfig[key])
+				var padding = ' '.repeat(Math.max(1, 15 - key.length))
+				console.log('[' + resourceName + ']: ' + key + ':' + padding + value)
+			})
+			console.log()
+		}
+		try {
+			resourceConfig.$instance = resource.$init(resourceConfig)
+			resourceConfig.$instance = yield resourceConfig.$instance
+		} catch (e) {
+			if (!e instanceof TypeError) {
+				throw e
+			}
+		}
+	}
+	result.$env = env
+	return result
+})
