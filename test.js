bc155900366ebad1400855dd6144549faffab9b0
anuragCES <anurag.sharma@cesltd.com>
2016-09-25 03:25:26 +0530

    Completed code review changes
    - Removed extra dependencies
    - Removed configData as a file
    - Added generators to configure the noNodeConfig object
    - Added try catch to handle edge cases
    - Replaced test.plan to test.end

--- test.js
+++ test.js
@@ -1,36 +1,67 @@
 const test = require('tape')
 const noNodeConfig = require('./index')
 const co = require('co')
-const config = require('./testConfig/configData')
+
+// Mock config data
 const keys = ['mongo', 'redis']
-const PROCESS_ENV = process.env.NODE_ENV = 'test_env'
+const PROCESS_ENV = process.env.NODE_ENV = 'development'
+const config = {
+	"mongo": {
+		"default": {
+			"db": "test",
+			"port": 27017
+		},
+		"development": {
+			"host": "127.0.0.1"
+		},
+		"production": {
+			"db": "prod",
+			"host": "192.168.0.10"
+		}
+	},
+    "redis": {
+		"default": {
+			"port": 6379
+		},
+		"development": {
+			"host": "127.0.0.1"
+		},
+		"production": {
+			"host": "192.168.0.11"
+		}
+	}
+}
 
-const configObject = co(function* () {
-    return yield noNodeConfig({config})
-})
-
-test('Should initialize config object', (t) => { 
-    t.plan(3)
-    configObject.then((conf) => {
-        t.equal(typeof conf, 'object', 'Returned value is an object')
-        t.equal(Object.keys(conf).length, Object.keys(config).length + 1, 'Returned object has equal length as input')
+test('Should initialize config object', co.wrap(function* (t) {
+    try {
+        let result = yield noNodeConfig({config})
+        // assertions
+        t.equal(typeof result, 'object', 'Returned value is an object')
+        t.equal(Object.keys(result).length, Object.keys(config).length + 1, 'Returned object has equal length as input')
         let keyMatchSuccess = true
         keys.map((key) => {
-            if (!(key in conf)) {
+            if (!(key in result)) {
                 keyMatchSuccess = false
             }
         })
         t.equal(true, keyMatchSuccess, 'Contains all keys as input config object')
-    })
-})
+    } catch (e) {
+        t.equal(e, undefined, 'No error')
+    }
+    t.end()
+}))
 
-test('Should return correct values as passed input', (t) => { 
-    t.plan(4)
-
-    configObject.then((conf) => {
-        t.equal(conf.mongo.db, config.mongo.default.db, 'Has correct mongo db value')
-        t.equal(conf.mongo.port, config.mongo.default.port, 'Has correct mongo port value')
-        t.equal(conf.redis.port, config.redis.default.port, 'Has correct redis port value')
-        t.equal(conf.env, PROCESS_ENV, 'Has correct PROCESS_ENV')
-    })
-})
+test('Should return correct values as passed input', co.wrap(function* (t) {
+    try {
+        let result = yield noNodeConfig({config})
+        // assertions
+        t.equal(result.mongo.db, config.mongo.default.db, 'Has correct mongo db value')
+        t.equal(result.mongo.port, config.mongo.default.port, 'Has correct mongo port value')
+        t.equal(result.redis.port, config.redis.default.port, 'Has correct redis port value')
+        t.equal(result.env, PROCESS_ENV, 'Has correct PROCESS_ENV')
+    }
+    catch (e) {
+        t.equal(e, undefined, 'No error')
+    }
+    t.end()
+}))
